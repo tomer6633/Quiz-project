22055b6e09603eff3c3bf077d7871292507bec90
ShlomiJAVA <36505901+ShlomiJAVA@users.noreply.github.com>
2023-02-22 11:00:10 +0200

    wip

--- test-3/dist/index.js
+++ test-3/dist/index.js
@@ -1,12 +1,28 @@
-var boxes = document.querySelectorAll('.box');
-var scoreDisplay = document.querySelector('#score');
-var first;
-var second;
-var matchCounter = 0;
-var score = 0;
+// let boxes = document.querySelectorAll(".level1")
+function selectLevel(ev) {
+    debugger;
+    try {
+        let boxes = '';
+        const ChosenLevel = ev.target.elements.levels.value;
+        if (ChosenLevel == "level1")
+            boxes = document.querySelectorAll(".level1");
+        if (ChosenLevel == "level2")
+            boxes = document.querySelectorAll(".level2,.level1");
+        if (ChosenLevel == "level3")
+            boxes = document.querySelectorAll(".level3,.level2,.level1");
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+const scoreDisplay = document.querySelector('#score');
+let first;
+let second;
+let matchCounter = 0;
+let score = 0;
 boxes.forEach(function (box) {
     // מסדר את הbox במסך בסדר אקראי לפי מספר 
-    var randomNum = Math.floor(Math.random() * 12);
+    let randomNum = Math.floor(Math.random() * 12);
     box.style.order = randomNum;
     scoreDisplay.textContent = "score:" + score;
     box.addEventListener('click', function Game() {
@@ -18,20 +34,20 @@ boxes.forEach(function (box) {
             second = box;
             box.classList.add('show');
             if (first.innerHTML === second.innerHTML) {
-                // first.style.pointerEvents = 'none';
-                // second.style.pointerEvents = 'none';
+                first.style.pointerEvents = 'none';
+                second.style.pointerEvents = 'none';
                 first = null;
                 second = null;
                 matchCounter++;
                 score += 10;
                 scoreDisplay.textContent = "score:" + score;
                 if (matchCounter >= 6)
-                    setTimeout(function () { return alert("Game done! Your score is " + score + " Refresh page to replay."); }, 2000);
+                    setTimeout(() => alert(`Game done! Your score is ${score} Refresh page to replay.`), 2000);
             }
             else {
                 first.classList.add('hide');
                 second.classList.add('hide');
-                setTimeout(function () {
+                setTimeout(() => {
                     first.classList.remove('show');
                     second.classList.remove('show');
                     first.classList.remove('hide');
--- test-3/index.ts
+++ test-3/index.ts
@@ -1,4 +1,20 @@
-const boxes = document.querySelectorAll('.box');
+// let boxes = document.querySelectorAll(".level1")
+
+function selectLevel(ev){
+    debugger;
+    try {
+        let boxes:any=''
+        const ChosenLevel = ev.target.elements.levels.value;
+        if (ChosenLevel=="level1") boxes = document.querySelectorAll(".level1");
+        if (ChosenLevel=="level2") boxes = document.querySelectorAll(".level2,.level1");
+        if (ChosenLevel=="level3") boxes = document.querySelectorAll(".level3,.level2,.level1");
+      
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
 const scoreDisplay:any = document.querySelector('#score');
 
 
@@ -9,10 +25,12 @@ let score = 0;
 
 boxes.forEach(function(box) {
     // מסדר את הbox במסך בסדר אקראי לפי מספר 
-    let randomNum = Math.floor(Math.random() * 12);        
+    let randomNum = Math.floor(Math.random() * 12);   
+  
     box.style.order = randomNum;
     scoreDisplay.textContent = "score:" + score;
     box.addEventListener('click', function Game() {
+    
         if (!first && !second) {
             first = box;
             box.classList.add('show');
@@ -20,8 +38,8 @@ boxes.forEach(function(box) {
             second = box;
             box.classList.add('show');
             if (first.innerHTML === second.innerHTML) {
-                // first.style.pointerEvents = 'none';
-                // second.style.pointerEvents = 'none';
+                first.style.pointerEvents = 'none';
+                second.style.pointerEvents = 'none';
                 first = null;
                 second = null;
                 matchCounter++;
