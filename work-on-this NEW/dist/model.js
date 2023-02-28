445f9a4fd83c585dd8375e166e333371c6891031
ShlomiJAVA <36505901+ShlomiJAVA@users.noreply.github.com>
2023-02-28 17:42:10 +0200

    score board by shlomi

--- work-on-this NEW/dist/model.js
+++ work-on-this NEW/dist/model.js
@@ -1,9 +1,11 @@
-var Player = /** @class */ (function () {
-    function Player(name, date, picture) {
+class Player {
+    constructor(name, date, picture, score) {
         this.name = name;
         this.date = date;
         this.picture = picture;
+        this.score = score;
         this.uid = uid();
     }
-    return Player;
-}());
+}
+const players = [];
+players.push(new Player("shlomi", new Date('2023-09-24'), "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYLzUgk9whjvWgo8rqMqCBlqG4Cepm2LSmx8fyrPkXqw&s", 56));
