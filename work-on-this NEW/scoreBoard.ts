f155400aad612dd7170768983a54689dacd2d23c
ShlomiJAVA <36505901+ShlomiJAVA@users.noreply.github.com>
2023-02-28 18:04:08 +0200

    Update scoreBoard.ts

--- work-on-this NEW/scoreBoard.ts
+++ work-on-this NEW/scoreBoard.ts
@@ -27,30 +27,3 @@ renderScoreBoard(players)
 
 
 
-// function renderItems(items: Item[]): string {
-//     try {
-//       if (!items || !Array.isArray(items))
-//         throw new Error("items is not an array");
-  
-//       const html = items
-//         .map((item) => {
-//           console.log(`---${item.uid}---`);
-//           return `
-//       <div class="item" style="background-color:${item.color}">
-//         <h3>${item.name}</h3>
-//         <div>Price: ${item.price} <button onclick="handleUpdatePrice()">Update</button></div>
-//         <div>Category: ${item.category}</div>
-//         <div>Size: ${item.size}</div>
-//         <div>S/N: ${item.sn}</div>
-//         <button onclick="handleDeleteItem('${item.uid}')">Remove</button>
-//       </div>
-//       `;
-//         })
-//         .join(" ");
-//       console.log(html);
-//       return html;
-//     } catch (error) {
-//       console.error(error);
-//       return "";
-//     }
-//   }
